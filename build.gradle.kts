6da2a09ece6baa298826abd748610ff862abefe6
Lars Eckart <lars.eckart@hey.com>
2022-04-07 18:41:01 +0300

    update junit5 to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 dependencies {
     api("org.junit.jupiter:junit-jupiter-api:5.7.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 val test by tasks.getting(Test::class) {
